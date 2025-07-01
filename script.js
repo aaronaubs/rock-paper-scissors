9bfac263568649b4a5b9fd6cad5e23b3737384fb
aaronaubs <aaubs4@gmail.com>
2025-07-01 19:55:37 -0400

    Define function to get human choice and log input
    Use prompt() to get human input and log lowercased
    version to the console. Change button onclick
    attribute to trigger prompt for user input.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* 
 Requirements for this project:
 1. Create HTML page (check!)
-2. Write logic to get computer choice
+2. Write logic to get computer choice (check!)
 3. Write logic to get human choice
 4. Declare score variables and enable them to track score to best of 5
 5. Write logic to play a round
@@ -10,7 +10,7 @@ Requirements for this project:
 */
 //Declare a global variable called computerChoice
 let computerChoice
-//Declare a function that will get a random computer choice
+//Define a function that will get a random computer choice
 function getComputerChoice() {
     //declare a variable called randomNumber
     //and set it to randomly choose either 0, 1 or 2.
@@ -26,4 +26,16 @@ function getComputerChoice() {
         computerChoice = "scissors";
     }
     console.log(computerChoice);
+}
+
+//Declare a global variable called humanChoice
+let humanChoice;
+//Define a function that gets the choice from a user input
+function getHumanChoice() {
+    //declare a variable that will hold the response to the prompt
+    let userInput = prompt("Please choose rock, paper or scissors");
+    //set humanChoice to equal a lowercased version of the user input
+    humanChoice = userInput.toLowerCase();
+    //log the lowercased input to the console
+    console.log(humanChoice);
 }
