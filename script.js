bd4e1cefe97a6fadf1032ed64fdca86af93dd8ef
aaronaubs <aaubs4@gmail.com>
2025-07-06 18:03:43 -0400

    Add response entry verification, change background to white
    I added typing verification to avoid handling of
    invalid responses. Set white background to
    brighten up page.

--- script.js
+++ script.js
@@ -1,88 +1,64 @@
-/* 
-Requirements for this project:
-1. Create HTML page (check!)
-2. Write logic to get computer choice (check!)
-3. Write logic to get human choice (check!)
-4. Declare score variables and enable them to track scores (check!)
-5. Write logic to play a round (check!)
-6. Write logic to play the entire game (check!)
-
-*/
-//Declare a global variable called computerChoice
 let computerChoice
-//Define a function that will get a random computer choice
 function getComputerChoice() {
-    //declare a variable called randomNumber
-    //and set it to randomly choose either 0, 1 or 2.
     let randomNumber = Math.floor(Math.random() * 3);
-    // make the function return the string "rock if the value is 0"
+    // make the function return "rock" if the value is 0
     if (randomNumber === 0) {
         computerChoice = "rock";
-    //paper if the value is 1    
+    //"paper" if the value is 1    
     } else if (randomNumber === 1) {
         computerChoice = "paper";
-    //and scissors if the value is 2 (neither 0 nor 1)    
+    //or "scissors" if the value is 2 (neither 0 nor 1)    
     } else {
         computerChoice = "scissors";
     }
     return computerChoice;
 }
 
-//Declare a global variable called humanChoice
-let humanChoice;
-//Define a function that gets the choice from a user input
-function getHumanChoice() {
-    //declare a variable that will hold the response to the prompt
+let playerChoice;
+function getPlayerChoice() {
     let userInput = prompt("Please choose rock, paper or scissors");
-    //set humanChoice to equal a lowercased version of the user input
-    humanChoice = userInput.toLowerCase();
-    //log the lowercased input to the console
-    return humanChoice;
+    playerChoice = userInput.toLowerCase();
+    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+        return playerChoice;
+    } else {
+        while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
+            playerChoice = prompt("Please check you've entered your response correctly").toLowerCase();
+        }
+        return playerChoice;
+    }
 }
 
-
-
-//Define playGame function
 function playGame() {
-    //declare variables to track the human score and computer score
-    let humanScore = 0;
+    let playerScore = 0;
     let computerScore = 0;
     
-    //while loop the game so it repeats until one side gets to 5 points
-    while (humanScore < 5 && computerScore < 5) {
-        //Define a function to play a single round of rock, paper scissors
-        function playRound(humanChoice, computerChoice) {
-            //check whether the round has ended in a draw
-            if (humanChoice === computerChoice) {
-                console.log(`You both chose ${humanChoice}. It's a draw!`);
-                console.log(`The score is still Player: ${humanScore} to Computer: ${computerScore}.`)
-            //if not a draw, check whether the user beat the computer
+    while (playerScore < 5 && computerScore < 5) {
+        function playRound(playerChoice, computerChoice) {
+            if (playerChoice === computerChoice) {
+                console.log(`You chose ${playerChoice} and the computer chose ${computerChoice}. It's a draw!`);
+                console.log(`The score is still Player: ${playerScore} to Computer: ${computerScore}.`)
             } else if (
-                (humanChoice === "rock" && computerChoice === "scissors") ||
-                (humanChoice === "paper" && computerChoice === "rock") ||
-                (humanChoice === "scissors" && computerChoice === "paper")
+                (playerChoice === "rock" && computerChoice === "scissors") ||
+                (playerChoice === "paper" && computerChoice === "rock") ||
+                (playerChoice === "scissors" && computerChoice === "paper")
             ) {
-                //announce the user's victory
-                console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
-                //increase user score by 1
-                humanScore++;
-                console.log(`The score is now Player: ${humanScore} to Computer: ${computerScore}.`)
-            //if the user did not win, announce that the user lost
+                console.log(`You chose ${playerChoice} and the computer chose ${computerChoice}. You win, ${playerChoice} beats ${computerChoice}!`);
+                playerScore++;
+                console.log(`The score is now Player: ${playerScore} to Computer: ${computerScore}.`)
             } else {
-                console.log(`You lose, ${computerChoice} beats ${humanChoice}!`)
-                //increase computer score by 1
+                console.log(`You chose ${playerChoice} and the computer chose ${computerChoice}. You lose, ${computerChoice} beats ${playerChoice}!`)
                 computerScore++;
-                console.log(`The score is now Player: ${humanScore} to Computer: ${computerScore}.`)
+                console.log(`The score is now Player: ${playerScore} to Computer: ${computerScore}.`)
             }
         }
 
-        let humanSelection = getHumanChoice();
+        let playerSelection = getPlayerChoice();
         let computerSelection = getComputerChoice();
             
-        playRound(humanSelection, computerSelection);  
+        playRound(playerSelection, computerSelection);  
     }
-    if (humanScore === 5) {
-        console.log("You win the game!");
+    if (playerScore === 5) {
+        console.log("You won the game! Great work!");
     } else {
         console.log("You lose! Better luck next time.");
     }
