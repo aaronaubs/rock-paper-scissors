855758337c43b0e508106d46d8ddfdabdcb2b65d
aaronaubs <aaubs4@gmail.com>
2025-07-09 19:31:37 -0400

    Add js logic, adjust html & css for compatibility
    Added js functions to play game in real time.
    Changed html tag id's and other values to improve
    compatibility with js code. Removed img tags.

--- script.js
+++ script.js
@@ -1,65 +1,73 @@
+let playerChoice;
 let computerChoice
+const choices = document.querySelectorAll(".choice");
+const gameStatusDisplay = document.getElementById("gameStatusDisplay");
+let playerScorecard = document.getElementById("playerScorecard");
+let computerScorecard = document.getElementById("computerScorecard");
+let playerScore = playerScorecard.dataset.value;
+let computerScore = computerScorecard.dataset.value;
+
+function appendParagraph(text) {
+    let para = document.createElement("p");
+    para.textContent = text;
+    gameStatusDisplay.appendChild(para);
+    gameStatusDisplay.scrollTo({
+        top: gameStatusDisplay.scrollHeight,
+        behavior: "smooth"
+    }); 
+}
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
-    // make the function return "rock" if the value is 0
-    if (randomNumber === 0) {
-        computerChoice = "rock";
-    //"paper" if the value is 1    
-    } else if (randomNumber === 1) {
-        computerChoice = "paper";
-    //or "scissors" if the value is 2 (neither 0 nor 1)    
-    } else {
-        computerChoice = "scissors";
-    }
+    if (randomNumber === 0) computerChoice = "rock";
+    if (randomNumber === 1) computerChoice = "paper";
+    if (randomNumber === 2) computerChoice = "scissors";
+    appendParagraph(`The computer chose ${computerChoice}.`);
     return computerChoice;
 }
 
-let playerChoice;
-function getPlayerChoice() {
-    let userInput = prompt("Please choose rock, paper or scissors");
-    playerChoice = userInput.toLowerCase();
-    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
-        return playerChoice;
+
+function determineRoundWinner() {
+    let roundCounter = document.getElementById("roundCounter");
+    roundCounter.dataset.value++;
+    roundCounter.textContent = roundCounter.dataset.value;
+
+    if (playerChoice === computerChoice) {
+        appendParagraph(`It's a draw.`);
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        appendParagraph(`You won the round!`);
+        playerScore++;
+        playerScorecard.textContent = playerScore;
     } else {
-        while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
-            playerChoice = prompt("Please check you've entered your response correctly").toLowerCase();
-        }
-        return playerChoice;
+        appendParagraph(`You lost the round!`)
+        computerScore++;
+        computerScorecard.textContent = computerScore;
     }
 }
 
-function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    while (playerScore < 5 && computerScore < 5) {
-        function playRound(playerChoice, computerChoice) {
-            if (playerChoice === computerChoice) {
-                console.log(`You chose ${playerChoice} and the computer chose ${computerChoice}. It's a draw!`);
-                console.log(`The score is still Player: ${playerScore} to Computer: ${computerScore}.`)
-            } else if (
-                (playerChoice === "rock" && computerChoice === "scissors") ||
-                (playerChoice === "paper" && computerChoice === "rock") ||
-                (playerChoice === "scissors" && computerChoice === "paper")
-            ) {
-                console.log(`You chose ${playerChoice} and the computer chose ${computerChoice}. You win, ${playerChoice} beats ${computerChoice}!`);
-                playerScore++;
-                console.log(`The score is now Player: ${playerScore} to Computer: ${computerScore}.`)
-            } else {
-                console.log(`You chose ${playerChoice} and the computer chose ${computerChoice}. You lose, ${computerChoice} beats ${playerChoice}!`)
-                computerScore++;
-                console.log(`The score is now Player: ${playerScore} to Computer: ${computerScore}.`)
-            }
-        }
-
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-            
-        playRound(playerSelection, computerSelection);  
-    }
+function checkGameWinner() {
     if (playerScore === 5) {
-        console.log("You won the game! Great work!");
-    } else {
-        console.log("You lose! Better luck next time.");
-    }
+        appendParagraph(`You won the game! Great work!.`)
+    } else if (computerScore === 5) {
+        appendParagraph(`You lost the game... Better luck next time.`)
+    } else return;
 }
+
+
+function playRound() {
+    choices.forEach(choice => {
+        choice.addEventListener("click", (e) => {
+            playerChoice = e.target.id;
+            appendParagraph(`You chose ${playerChoice}.`);
+            getComputerChoice();
+            determineRoundWinner();
+            checkGameWinner();
+        });
+    });
+}
+
+playRound();
